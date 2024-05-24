eb84135d8ffab5720a6b54a8b52489133614fa34
ROJAS CENEDESE MARTINA <47695905@ort>
2024-05-24 12:11:00 -0300

    primera req

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+import express from 'express';
 
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rjujpbbzlfzfemavnumo.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = 'https://rjujpbbzlfzfemavnumo.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqdWpwYmJ6bGZ6ZmVtYXZudW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNTg2MzE5OCwiZXhwIjoyMDMxNDM5MTk4fQ.9GoC2ZHaoV5gjq_Y0H84FQ_cbhhkRzFSiIWmTeQG-RU';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const app = express();
+app.use(express.json());
+
+app.get('/estudios', async (req,res)=> {
+    
+    const { data, error } = await supabase
+        .from('Estudios')
+        .select('*');
+
+    await res.send(data);
+})
+app.post('/estudios', async (req,res)=> {
+    
+    const body = req.body;
+    const { data, error } = await supabase
+        .from('Estudios')
+        .select('*');
+
+    await res.send(data);
+})
+
+app.listen(3000,() => {
+    console.log("Server running");
+})

5cf9680e6eb196a32025a5a563d427f7ff4327f0
ROJAS CENEDESE MARTINA <47695905@ort>
2024-06-03 16:56:47 -0300

    arreglar insert estudios

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import multer from 'multer';
 import { createClient } from '@supabase/supabase-js';
 import { v4 as uuidv4 } from 'uuid';
 import cors from "cors";
-
+import dotenv from 'dotenv'
+dotenv.config();
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const supabaseUrl = 'https://rjujpbbzlfzfemavnumo.supabase.co';
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqdWpwYmJ6bGZ6ZmVtYXZudW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNTg2MzE5OCwiZXhwIjoyMDMxNDM5MTk4fQ.9GoC2ZHaoV5gjq_Y0H84FQ_cbhhkRzFSiIWmTeQG-RU";
 const supabase = createClient(supabaseUrl, supabaseKey);
@@ -15,7 +17,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 async function insertToSupabase(table, values){
-    const { error } = await supabase
+    const error = await supabase
             .from(table)
             .insert(values);
     return error;
@@ -68,8 +70,12 @@ app.get('/estudios', async (req, res) => {
 app.post('/estudio', upload.single('file'), async (req, res) => {
     const file = req.file;
     const body = req.body;
-    console.log("Post");
-    res.send(req.body);
+    console.log(body, file, {
+        archivo_estudios: "publicURL", 
+        tipo_estudios: body.tipo,
+        fecha_estudios: body.date,
+        quien_subio_estudios: body.quien_subio,
+        id_usuarios: body.usuario});
     if (!file) {
         return res.status(400).send('No file uploaded.');
     }
@@ -83,13 +89,19 @@ app.post('/estudio', upload.single('file'), async (req, res) => {
     if (!publicURL) {
         return res.status(500).send('Error uploading file to Supabase.');
     }
-    const error_insert = insertToSupabase("Estudios", {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+    //Cambiar por seguridad
+    const error_insert = await insertToSupabase("Estudios", {
         archivo_estudios: publicURL, 
-        tipo_estudio: body.tipo,
-        fecha_estudio: body.date,
+        tipo_estudios: body.tipo,
+        fecha_estudios: body.date,
         quien_subio_estudios: body.quien_subio,
-        id_usuario: body.usuario}); 
-    
+        id_usuarios: body.usuario}); 
+    if(error_insert){
+        console.log("Error insertando el archivo: ", error_insert);
+        return res.status(500).send('Error inserting data');
+
+    }
     // res.send(`File uploaded successfully. URL: ${publicURL}`);
 });
 

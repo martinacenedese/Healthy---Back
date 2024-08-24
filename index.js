d98dd9417d6f0e6111482e185c08cbecd02b6b32
LucasBehrend <lucasbehrend2006@gmail.com>
2024-08-23 22:00:27 -0300

    arreglar fimcopm authenticate toke2n

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function authenticateToken (req, res, next) {
     if (token === null) return res.sendStatus(401);
     
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,id) => {
-        if (err) return res.sendStatus(403).send(err); //token expiration
+        if (err) return res.status(403).send(err); //token expiration
         req.id = id;
         next();
     })

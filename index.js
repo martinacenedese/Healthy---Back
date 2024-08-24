10b2a88161243552f4d09d858cd43d05e616c9fa
LucasBehrend <lucasbehrend2006@gmail.com>
2024-08-23 21:59:05 -0300

    arreglar fimcopm authenticate token

--- index.js
+++ index.js
@@ -92,15 +92,15 @@ async function userURL(id, url) {
 
 function authenticateToken (req, res, next) {
     //Bearer token
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
+    const authHeader = req.headers['Authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
 
-    if (token === null) return res.sendStatus(401)
+    if (token === null) return res.sendStatus(401);
     
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,id) => {
-        if (err) return res.sendStatus(403) //token expiration
-        req.id = id
-        next()
+        if (err) return res.sendStatus(403).send(err); //token expiration
+        req.id = id;
+        next();
     })
 }
 
